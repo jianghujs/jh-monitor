65d6b08286576dde5f25afa56ca182644b92fb7d
Jam <yedsn@163.com>
2024-11-08 14:23:32 +0800

    安装脚本细节调整

--- scripts/client/install.sh
+++ scripts/client/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 安装脚本：bash /www/server/jh-monitor/scripts/install/install_client/install_client.sh  install  http://192.168.7.73:10844
+# 安装脚本：bash /www/server/jh-monitor/scripts/client/install.sh  install  http://192.168.7.73:10844
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
@@ -12,6 +12,24 @@ AUTHORIZED_KEYS="$SSH_DIR/authorized_keys"
 # 确保 target_dir 和 download_Url 被定义
 target_dir="/usr/local/btmonitoragent"
 download_Url="http://example.com"  # 请替换为实际的下载地址
+
+
+prompt()
+{
+  tip=$1
+  local _resultvar=$2
+  local default_choice=$3
+  echo -ne "\033[1;32m?\033[0m \033[1m${tip}\033[0m"
+  read choice
+  choice=${choice:-$default_choice}
+  eval $_resultvar="'$choice'"
+}
+
+show_error()
+{
+  tip=$1
+  echo -e "\033[1;31m× ${tip}\033[0m"
+}
 
 Command_Exists() {
     command -v "$@" >/dev/null 2>&1
@@ -202,8 +220,23 @@ Setup_SSH_Config(){
 }
 
 Add_Host_To_Monitor(){
-    CLIENT_IP=$(hostname -I | awk '{print $1}')
-    add_res=$(curl -s -X POST "$monitor_url/pub/add_host" -d "host_name=$CLIENT_IP" -d "ip=$CLIENT_IP")
+    
+    default_client_ip=$(hostname -I | awk '{print $1}')
+    prompt "请输入服务端连接到当前机器的IP（默认为：${default_client_ip}）：" client_ip $default_client_ip
+    if [ -z "$client_ip" ]; then
+      show_error "错误:未指定本地IP地址"
+      exit 1
+    fi
+
+    # 获取客户端名称
+    default_client_name=$(hostname -s)
+    prompt "请输入客户端名称（默认为：${default_client_name}）：" client_name $default_client_name
+    if [ -z "$client_name" ]; then
+      show_error "错误:未指定客户端名称"
+      exit 1
+    fi
+
+    add_res=$(curl -s -X POST "$monitor_url/pub/add_host" -d "host_name=$client_name" -d "ip=$client_ip")
     echo $add_res
     # 判断返回的json中status是否为true
     if [[ "$add_res" =~ "true" ]]; then
@@ -241,7 +274,7 @@ elif [ "$action" == "install" ]; then
     Setup_SSH_Config
 
     # 通知服务端添加主机
-    Add_Host_To_Monitor
+    f
     # Download_Agent
     # Set_Crontab
     # Timezones_Check
