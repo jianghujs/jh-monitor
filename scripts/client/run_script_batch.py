021fbb21d6ed5d1fc644def7b52945b2f1614aa4
Jam <yedsn@163.com>
2024-11-22 19:29:30 +0800

    在临时目录下执行ansible

--- scripts/client/run_script_batch.py
+++ scripts/client/run_script_batch.py
@@ -1,5 +1,6 @@
 import ansible_runner
 import json
+import tempfile
 
 def run_script_batch(script_names):
     print("Running scripts:", script_names)
@@ -7,44 +8,46 @@ def run_script_batch(script_names):
     playbook_path = f'{jh_monitor_path}scripts/client/run_script_batch.yml'  # 替换为你的 Playbook 文件路径
     
     # 使用 ansible-runner 执行 Playbook
-    r = ansible_runner.run(
-        private_data_dir=f'{jh_monitor_path}/data', 
-        playbook=playbook_path,
-        extravars={'script_names': script_names}  # 传递参数列表
-    )
+    
+    with tempfile.TemporaryDirectory() as tmpdir:
+        r = ansible_runner.run(
+            private_data_dir=tmpdir, 
+            playbook=playbook_path,
+            extravars={'script_names': script_names}  # 传递参数列表
+        )
 
-    if r.status == 'successful':
-        print("Playbook executed successfully")
-    else:
-        print("Playbook execution failed")
+        if r.status == 'successful':
+            print("Playbook executed successfully")
+        else:
+            print("Playbook execution failed")
 
-    result = {}
-    for event in r.events:
-        if event.get('event') == 'runner_on_ok':
-            event_data = event.get('event_data', {})
-            task_name = event_data.get('task', '')
-            
-            if task_name == 'Return Results':
+        result = {}
+        for event in r.events:
+            if event.get('event') == 'runner_on_ok':
+                event_data = event.get('event_data', {})
+                task_name = event_data.get('task', '')
                 
-                # IP地址
+                if task_name == 'Return Results':
+                    
+                    # IP地址
+                    ip = event_data.get('host', '')
+                    # 返回结果
+                    res = event_data.get('res', {})
+                    r = {item.get('msg', '')[0]: item.get('msg', '')[1] for item in res.get('results', [])}
+                    
+                    result[ip] = {
+                        'status': 'ok',
+                        'data': r
+                    }
+            elif event.get('event') == 'runner_on_unreachable': 
+                event_data = event.get('event_data', {})
                 ip = event_data.get('host', '')
-                # 返回结果
-                res = event_data.get('res', {})
-                r = {item.get('msg', '')[0]: item.get('msg', '')[1] for item in res.get('results', [])}
-                
                 result[ip] = {
-                    'status': 'ok',
-                    'data': r
+                    'status': 'fail',
+                    'msg': 'unreachable'
                 }
-        elif event.get('event') == 'runner_on_unreachable': 
-            event_data = event.get('event_data', {})
-            ip = event_data.get('host', '')
-            result[ip] = {
-                'status': 'fail',
-                'msg': 'unreachable'
-            }
 
-    return result
+        return result
 
 # if __name__ == "__main__":
 #     script_outputs = run_script_batch(['get_host_info.py', 'get_host_usage.py'])
